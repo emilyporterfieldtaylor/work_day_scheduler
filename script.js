ca0e0024102269cda1555edb908e0a846c6b59bb
Emily Taylor <emilyporterfieldtaylor@gmail.com>
2020-01-21 15:03:49 -0600

    create .time-block hour validation function to change the classes of the textareas

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 jQuery(document).ready(function($){
 
-    $("#currentDay").moment().format('MMMM Do YYYY');
+    var currentDayEl = document.querySelector("#currentDay");
+    currentDayEl.textContent = moment().format('MMMM Do YYYY');
 
-    /*var now = moment();
+    //should I make time blocks a string so I can traverse them easier?
+    //should I add values to the time blocks and pass the vals in instead of the text?
+    //What data type does moment use?
 
-    $('time').each(function(i, e) {
-        var time = moment($(e).attr('datetime'));
+    //add in if statement to vaild what time it is and change css based on hour
 
-        if(now.diff(time, 'days') <= 1) {
-            $(e).html('<span>' + time.from(now) + '</span>');
-        }
-    });*/
+    //for (var i = 0; i < blockHourEl.length; i++) {
+        console.log($(".time-block"));
+    // $(".time-block").each(element => {
+    //     if (testEl > element[0].id) {
+    //         element.addClass("past");
+    //     } else if (testEl = blockHourEl) {
+    //         element.addClass("present");
+    //     } else {
+    //         element.addClass("future");
+    //     }
+    //});
 
+    var currentHourEl = moment().format('HH');
+
+       $(".time-block").each(function (index, element) {
+            if (currentHourEl > $(element).attr("id")) {
+                $(element).addClass("past");
+            } else if (currentHourEl === $(element).attr("id")) {
+                $(element).addClass("present");
+            } else {
+                $(element).addClass("future");
+            }
+       });
+
+      // console.log($(element));
+//};
+    //add local storge saving 
 });
-
-//$(".lead").hide();
