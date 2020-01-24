8340ea09738fcb5d53bd1c50253ed876cf9dc71d
Emily Taylor <emilyporterfieldtaylor@gmail.com>
2020-01-23 18:31:37 -0600

    create readme.md content, code ass to script.js to attempt to save to local storage, minor css changes

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # work_day_scheduler
+
+Is your desk covered in loose paper and sticky-notes? Is your boss always hounding you because you forgot about the big project you were supposed to have completed by Monday?
+
+Look no further! This is a "state-of-the-art" task scheduler. The current hour is highlighted in a beautiful lavender. You are able to put in any task and save it for the day. 
+
+Test it out and show your boss how organized you are!
+
+## Languages
+This application is made with HTML, CSS, and Javascript. 
+
+## Reference Libraries:
+* Font Awesome
+* Bootstrap
+* jQuery
+* Moment.js
+
+## Instructions
+1. Type your task into the colored text area
+![alt text](asset/submittask.png "start")
+2. Click on the save button when you are done writing your task
+![alt text](asset/selectsave.png "start")
+3. Your submitted tasks will be there even if you refresh the browser. Go ahead! Try it!
+
+## Authors
+
+* **Emily Taylor** - *Initial work* - [emilyporterfieldtaylor](https://github.com/emilyporterfieldtaylor)
+
+## License
+
+This project is licensed under the MIT License 
+
+## Acknowledgments
+
+* Tyler Wood - my helpful roommate
+* Leandra - for helping me with the local storage bits
--- script.js
+++ script.js
@@ -3,27 +3,11 @@ jQuery(document).ready(function($){
     var currentDayEl = document.querySelector("#currentDay");
     currentDayEl.textContent = moment().format('MMMM Do YYYY');
 
-    //should I make time blocks a string so I can traverse them easier?
-    //should I add values to the time blocks and pass the vals in instead of the text?
-    //What data type does moment use?
-
-    //add in if statement to vaild what time it is and change css based on hour
-
-    //for (var i = 0; i < blockHourEl.length; i++) {
-        console.log($(".time-block"));
-    // $(".time-block").each(element => {
-    //     if (testEl > element[0].id) {
-    //         element.addClass("past");
-    //     } else if (testEl = blockHourEl) {
-    //         element.addClass("present");
-    //     } else {
-    //         element.addClass("future");
-    //     }
-    //});
-
     var currentHourEl = moment().format('HH');
 
-       $(".time-block").each(function (index, element) {
+    var taskEL = document
+
+    $(".time-block").each(function (index, element) {
             if (currentHourEl > $(element).attr("id")) {
                 $(element).addClass("past");
             } else if (currentHourEl === $(element).attr("id")) {
@@ -33,9 +17,11 @@ jQuery(document).ready(function($){
             }
        });
 
+       //add local storage saving 
        $("button").click(function(){
         alert ();
+        localStorage.setItem("task", JSON.stringify(window.HTMLTextAreaElement));
        });
 
-    //add local storge saving 
+
 });
